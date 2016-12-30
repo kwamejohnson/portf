aa355ad535a5fd05419d95b20180a7c5dce24d88
kwamejohnson <kwamejohnson@alumni.duke.edu>
2016-12-30 18:41:59 -0400

    portfolio site 2nd

--- client/projectTechController.js
+++ client/projectTechController.js
@@ -1,27 +1,74 @@
 app.controller('projectTechController', ['$scope', function($scope){
 
-console.log("in projectTecController");
+console.log("in projectTechController");
 // $scope.projectTech = ["kwamejohnson.com", "polyglotty.com", "MorulaMed.com", "kwoding.com"]
 // $scope.technologies = ["JavaScript", "Python", "iOS", "SQL", "Angular 1"]
 
 $scope.message = "Where is the bug??";
 
-$scope.projectTech = { projects: ["kwamejohnson.com",
+
+$scope.projectTech = {
+                  name: [
+                    "kwamejohnson.com",
                     "PolyGlotty.com",
                     "MorulaMed.com",
-                    "MoviePlanner.com",
+                    "MovieKangaroo.com",
                     "kwoding.com",
                     "KlinicalTrials.com",
-                    "clearbuoyant.com"],
-                  technologies:[
-                      ["JavaScript", "Angular 1", "Bootstrap", "JQuery"],
-                      ["Full-Stack MEAN/JavaScript (Mongo, Express, NodeJS, Angular)"], ["Python", "Django"],
+                    "clearbuoyant.com"
+                  ],
+                  technology: [
+                      ["JavaScript", "Angular", "Bootstrap", "JQuery"],
+                      ["Full-Stack MEAN/JavaScript (Mongo, Express, NodeJS, Angular)"],
+                      ["Python", "Django"],
                       ["JavaScript", "d3", "React"],
                       ["Ruby on Rails"],
                       ["Python", "Web Scraping"],
                       ["C#", "ASP .NET"]
-                    ]}
+                  ],
+                  description: [
+                    "Portfolio website",
+                    "Programming concepts across multiple languages",
+                    "Bio-tech, med-tech, and health-tech startups",
+                    "Movie hopping made easy",
+                    "My personal coding journey blog",
+                    "Clinical Trial Search Portal in simple English",
+                    "Technology Company Website"
+                  ]
+                }
 
+
+// $scope.projects = [
+//                     "kwamejohnson.com",
+//                     "PolyGlotty.com",
+//                     "MorulaMed.com",
+//                     "MovieKangaroo.com",
+//                     "kwoding.com",
+//                     "KlinicalTrials.com",
+//                     "clearbuoyant.com"
+//                   ]
+//
+// $scope.technologies = [
+//                       ["JavaScript", "Angular", "Bootstrap", "JQuery"],
+//                       ["Full-Stack MEAN/JavaScript (Mongo, Express, NodeJS, Angular)"],
+//                       ["Python", "Django"],
+//                       ["JavaScript", "d3", "React"],
+//                       ["Ruby on Rails"],
+//                       ["Python", "Web Scraping"],
+//                       ["C#", "ASP .NET"]
+//                   ]
+//
+// $scope.descriptions = [
+//                     "Portfolio website",
+//                     "Programming concepts across multiple languages",
+//                     "Bio-tech, med-tech, and health-tech startups",
+//                     "Movie hopping made easy",
+//                     "My personal coding journey blog",
+//                     "Clinical Trial Search Portal in simple English",
+//                     "Technology Company Website"
+//                   ]
+
+}])
 // $scope.projects = ["kwamejohnson.com",
 //                     "PolyGlotty.com",
 //                     "MorulaMed.com",
@@ -55,5 +102,34 @@ $scope.projectTech = { projects: ["kwamejohnson.com",
 //
 // }
 // //
+
 //
-}])
+// $scope.projectTech = { projects:[
+//                     "kwamejohnson.com",
+//                     "PolyGlotty.com",
+//                     "MorulaMed.com",
+//                     "MovieKangaroo.com",
+//                     "kwoding.com",
+//                     "KlinicalTrials.com",
+//                     "clearbuoyant.com"
+//                   ],
+//                   technologies:[
+//                       ["JavaScript", "Angular", "Bootstrap", "JQuery"],
+//                       ["Full-Stack MEAN/JavaScript (Mongo, Express, NodeJS, Angular)"],
+//                       ["Python", "Django"],
+//                       ["JavaScript", "d3", "React"],
+//                       ["Ruby on Rails"],
+//                       ["Python", "Web Scraping"],
+//                       ["C#", "ASP .NET"]
+//                   ]
+//                   description:[
+//                     "Portfolio website",
+//                     "Programming concepts across multiple languages",
+//                     "Bio-tech, med-tech, and health-tech startups",
+//                     "Movie hopping made easy",
+//                     "My personal coding journey blog",
+//                     "Clinical Trial Search Portal in simple English",
+//                     "Technology Company Website"
+//                   ]
+//                   }
+//
